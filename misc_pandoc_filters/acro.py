6e0b22a55b068457ed2d41265a0d1fbb746f1713
Ed Bordin <edbordin@gmail.com>
2023-05-08 11:00:28 +1000

    Update acro.py

--- misc_pandoc_filters/acro.py
+++ misc_pandoc_filters/acro.py
@@ -1,25 +1,48 @@
+#!/usr/bin/python3
+# 
+# Simple filter that forces the author to define acronyms on first usage.
+# A link element is used to define the acronym the first time:
+# `[acro:TLA](Three Letter Acronym)`
+# 
+# For all subsequent uses of the acronym, use:
+# `[acro:TLA]()`
+# 
+# The filter will warn if multiple definitions are given and will throw an error if the acronym is never defined.
+# Optionally insert `$acronyms_list` into the document to generate a Definition List of acronyms used in the document
+
+
 from panflute import *
 import urllib.parse
 import sys
+import warnings
 
 #acronyms = set()
-definitions = {}
+__definitions = {}
+ACRO_TAG='acro:'
 
 # prints to stderr - useful for debugging
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
+def prepare(doc):
+    doc.backmatter = {}
+
 def acrolinks(elem, doc):
-    if type(elem) == Link:
+    if type(elem) == Link and type(elem.content[0]) == Str:
         linkref = elem.url
         acro = elem.content[0].text
-        if linkref.startswith('acro:'):
-            acrodef = urllib.parse.unquote(linkref[len('acro:'):])
-            if acro not in definitions:
-                definitions[acro] = acrodef
-                return split_spaces(acrodef) + [Space(), Str('('+acro+')')]
+        if acro.startswith(ACRO_TAG):
+            stripped_acro = acro[len(ACRO_TAG):]
+            acrodef = urllib.parse.unquote(linkref)
+            if stripped_acro not in doc.backmatter:
+                if len(acrodef) == 0:
+                    raise ValueError(f'Definition not given for first use of acronym: {stripped_acro}\nExample syntax: [acro:TLA](Three Letter Acronym)\n')
+                doc.backmatter[stripped_acro] = acrodef
+                return split_spaces(acrodef) + [Space(), Str('('+stripped_acro+')')]
             else:
-                return split_spaces(acro)
+                if len(acrodef) > 0:
+                    warnings.warn(f'Definition given for acronym that was already defined: {stripped_acro}\nExample syntax after first use: [acro:TLA]()\n')
+                return split_spaces(stripped_acro)
 
 def split_spaces(s):
     res = []
@@ -32,17 +55,17 @@ def split_spaces(s):
         res.append(Str(split[-1]))
     return res
 
-def build_def_list():
+def finalize(doc):
     items = []
-    for acro in definitions.keys():
-        defn = Definition(Para(*split_spaces(definitions[acro])))
+    for acro in sorted(doc.backmatter.keys()):
+        defn = Definition(Para(*split_spaces(doc.backmatter[acro])))
         items.append(DefinitionItem(term=[Str(acro)], definitions=[defn]))
-    return DefinitionList(*items)
+    doc = doc.replace_keyword('$acronyms_list', DefinitionList(*items))
 
 def main():
     doc = load()
-    doc = run_filter(acrolinks, doc=doc)
-    doc.content =  list(doc.content) + [build_def_list()]
+    doc = run_filter(acrolinks, prepare, finalize, doc=doc)
+    # doc.content =  list(doc.content) + [build_def_list()]
     dump(doc)
     
 if __name__ == "__main__":
